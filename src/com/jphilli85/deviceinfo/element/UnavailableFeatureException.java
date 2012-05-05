77662f324e55007132e99bc60fba3cc4559fc2e6
Jacob Phillips <jphilli85@gmail.com>
2012-05-05 04:26:28 -0400

    setup styles

--- src/com/jphilli85/deviceinfo/element/UnavailableFeatureException.java
+++ src/com/jphilli85/deviceinfo/element/UnavailableFeatureException.java
@@ -1,5 +1,7 @@
 package com.jphilli85.deviceinfo.element;
 
+import android.view.LayoutInflater;
+
 public class UnavailableFeatureException extends Exception {
 	private static final long serialVersionUID = 1L;
 	
